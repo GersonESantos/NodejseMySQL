c42a31936d38935866dc433c8f684669dc80fc02
GersonESS <gebhsantos@gmail.com>
2025-01-02 07:13:48 -0300

    a

--- Node.js e MySQL/app.js
+++ Node.js e MySQL/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 
 const app = express();
+const mysql = require('mysql'); 
+
+
+const connection = mysql.createConnection({
+    host: 'localhost', 
+    user: 'root',
+    password: 'Gabibi89*',
+    database: 'projeto'
+    });
+
+connection.connect(function(err) {
+    if(err) return console.log(err);
+    console.log('conectou!');
+    });
+
+
+    
 app.get('/', function(req, res) {   
     res.write('Hello World');
     res.end();
